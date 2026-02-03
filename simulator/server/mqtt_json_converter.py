0d5f0713a7fc44ed62e445ca8d07d67de69f88a4
PCBMoreira0 <pcbmoreira@id.uff.br>
2026-02-03 01:15:18 -0300

    feat: add new commands to mqtt-json converter

--- simulator/server/mqtt_json_converter.py
+++ simulator/server/mqtt_json_converter.py
@@ -13,6 +13,30 @@ class MqttJsonConverter:
                 "command": "listen",
                 "parameter": payload
             }
+        elif command == "AUDIO":
+            parameter = payload.split("|")
+            
+            return {
+                "command": "audio",
+                "parameter": [{"audio":parameter[0]}, {"block":parameter[1]}]
+            }
+        elif command == "LED":
+            return {
+                "command": "led",
+                "parameter": payload
+            }
+        elif command == "EVAEMOTION":
+            return {
+                "command": "emotion",
+                "parameter": payload
+            }
+        elif command == "LIGHT":
+            parameter = payload.split("|")
+
+            return {
+                "command": "light",
+                "parameter": [{"color":parameter[0]}, {"state":parameter[1]}]
+            }
         
     @staticmethod
     def json_to_mqtt(json_message : dict) -> tuple[str, str]:
